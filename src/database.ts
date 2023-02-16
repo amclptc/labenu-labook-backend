9a5f10cb41c608e76995858fa1cd40c54aa65937
Andersom Maciel <amclptc@gmail.com>
2023-02-16 01:50:08 -0300

    Labenu - Ammal B - Projeto Labook - Andersom Maciel - Validações

--- src/database.ts
+++ src/database.ts
@@ -2,4 +2,17 @@ import { TUser, TPost } from "./types";
 
 export const users: TUser[] = [];
 
-export const posts: TPost[] = [];
+export const posts: TPost[] = [
+    {
+        id: "p001",
+        content: "Hoje vou estudar POO!",
+        likes: 2,
+        dislikes: 1,
+        createdAt: "2023-01-20T12:11:47:000Z",
+        updatedAt: "2023-01-20T12:11:47:000Z",
+        creator: {
+            id: "uma uuid v4",
+            name: "Fulano"
+        }
+    }
+];
