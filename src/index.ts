d65b5f2c0b5b6afe5ef4fd3be6caf3cd32a30ccf
Andersom Maciel <amclptc@gmail.com>
2023-02-16 17:08:44 -0300

    Labenu - Ammal B - Projeto Labook - Andersom Maciel - Knex

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import  express, { Request, Response} from 'express';
 import cors from 'cors';
 import { TPost, TUser } from './types';
 import { posts, users } from './database';
+import { db } from "./database/knex";
 
 //cria o servidor do express:
 const app = express();
@@ -16,7 +17,7 @@ app.listen(3003, () => {
 
 
 //endpoint de teste:
-app.get('/ping', (req: Request, res: Response) => {
+app.get('/ping', async (req: Request, res: Response) => {
     try {
         res.send('Pong!')
 
@@ -27,13 +28,13 @@ app.get('/ping', (req: Request, res: Response) => {
   });
 
 //endpoint signup:
-app.post('/users/signup', (req: Request, res: Response) => {
+app.post('/users/signup', async (req: Request, res: Response) => {
     try {
         const name = req.body.name as string;
         const email = req.body.email as string;
         const password = req.body.password as string;
 
-        const emailExists = users.find((user) => user.email === email);
+        const emailExists = await db("users").where({email: email});
 
         if(emailExists){
             res.statusCode = 404;
@@ -46,9 +47,9 @@ app.post('/users/signup', (req: Request, res: Response) => {
             password
         }
 
-        users.push(newUser);
+        await db("users").insert(newUser);
     
-        res.status(200).send("Cadastro realizado com sucesso!");
+        res.status(200).send({message: "Cadastro realizado com sucesso!"});
         
     } catch (error:any) {
         console.log(error);
@@ -62,12 +63,12 @@ app.post('/users/signup', (req: Request, res: Response) => {
 });
 
 //endpoint login:
-app.post('/users/login', (req:Request, res:Response) => {
+app.post('/users/login', async (req:Request, res:Response) => {
     try {
         const email = req.body.email as string;
         const password = req.body.password as string;
 
-        const emailExists = users.find((user) => user.email === email);
+        const emailExists = await db("users").where({email: email});
 
         if(!emailExists){
             res.statusCode = 404;
@@ -86,9 +87,11 @@ app.post('/users/login', (req:Request, res:Response) => {
 });
 
 //endpoint get posts:
-app.get('/posts', (req:Request, res:Response) => {
+app.get('/posts', async (req:Request, res:Response) => {
     try {
-        res.status(200).send(posts);
+        const result = await db("posts");
+
+        res.status(200).send(result);
 
     } catch (error: any) {
 		console.log(error)
@@ -97,7 +100,7 @@ app.get('/posts', (req:Request, res:Response) => {
 });
 
 // endpoint create post:
-app.post('/posts', (req:Request, res:Response) => {
+app.post('/posts', async (req:Request, res:Response) => {
     try {
         // const auth = req.headers.authorization as string;
 
@@ -110,29 +113,34 @@ app.post('/posts', (req:Request, res:Response) => {
         const creatorId = req.body.creator.id as string;
         const creatorName = req.body.creator.name as string;
 
-        const idExists = posts.find((post) => post.id === id);
+        const idExists = await db("posts").where({id: id});
 
         if(idExists){
 			res.status(404)
-			throw new Error("Essa ID pertence a outro post!")
-        }
+			throw new Error("Essa ID pertence a outro post!");
+        };
 
-        const newPost: TPost = {
-            id,
-            content,
-            likes,
-            dislikes,
-            createdAt,
-            updatedAt,
+        if(!id || !content){
+            res.status(404)
+			throw new Error("Post incompleto!");
+        };
+
+        const newPost = {
+            id: id,
+            content: content,
+            likes: likes,
+            dislikes: dislikes,
+            createdAt: createdAt,
+            updatedAt: updatedAt,
             creator = {
-                creatorId,
-                creatorName
+                id: creatorId,
+                name: creatorName
             }
         }
 
-        posts.push(newPost);
-
-        res.status(200).send("Post criado com sucesso!");
+        await db("users").insert(newPost);
+        
+        res.status(200).send({message: "Post criado com sucesso!"});
         
     } catch (error:any) {
         console.log(error);
@@ -146,7 +154,7 @@ app.post('/posts', (req:Request, res:Response) => {
 });
 
 //endpoint edit post:
-app.put('/posts/:id', (req:Request, res:Response) => {
+app.put('/posts/:id', async (req:Request, res:Response) => {
     try {
         // const auth = req.headers.authorization as string;
         const id = req.params.id;
@@ -160,20 +168,28 @@ app.put('/posts/:id', (req:Request, res:Response) => {
         const creatorId = req.body.creator.id as string | undefined;
         const creatorName = req.body.creator.name as string | undefined;
 
-        const post = posts.find((post) => post.id === id);
+        const post = await db("posts").where({id: id});
 
         if(post){
-            post.id = newId || post.id;
-            post.content = content || post.content;
-            post.likes = isNaN(likes) ? post.likes : likes;
-            post.dislikes = isNaN(dislikes) ? post.dislikes : dislikes;
-            post.createdAt = createdAt || post.createdAt;
-            post.updatedAt = updatedAt || post.updatedAt;
-            post.creator.id = creatorId || post.creator.id;
-            post.creator.name = creatorName || post.creator.name;
-        }
+            const updatedPost = {
+                id: newId || post.id,
+                content: content || post.content,
+                likes: likes || post.likes,
+                dislikes: dislikes || post.dislikes,
+                createdAt: createdAt || post.createdAt,
+                updatedAt: updatedAt || post.updatedAt,
+                creatorId: creatorId || post.creatorId,
+                creatorName: creatorName || post.creatorName
+            };
 
-        res.status(200).send("Atualização realizada com sucesso!");
+            await db("posts").update(updatedPost).where({id: id});
+
+        } else {
+            res.status(404)
+            throw new Error("ID não encontrada!");
+        };
+
+        res.status(200).send({message: "Atualização realizada com sucesso!"});
         
     } catch (error:any) {
         console.log(error);
@@ -187,24 +203,21 @@ app.put('/posts/:id', (req:Request, res:Response) => {
 });
 
 //endpoint delete post:
-app.delete('/posts/:id', (req:Request, res:Response) => {
+app.delete('/posts/:id', async (req:Request, res:Response) => {
     try {
         // const auth = req.headers.authorization as string;
         const id = req.params.id;
-
-        const postIndex = posts.findIndex((post) => post.id === id);
         
-        const postExists = posts.find((post) => post.id === id);
+        const postExists = await db("posts").where({id: id});
+
         if(!postExists){
             res.status(404)
 			throw new Error("Post não encontrado!")
         }
 
-        if(postIndex>=0){
-            posts.splice(postIndex, 1);
-        }
+        await db("posts").del().where({id: id});
         
-        res.status(200).send("Post deletado com suceso!");
+        res.status(200).send({message: "Post deletado com suceso!"});
         
     } catch (error:any) {
         console.log(error);
@@ -218,7 +231,7 @@ app.delete('/posts/:id', (req:Request, res:Response) => {
 });
 
 //endpoint like/dislike post:
-app.put('/posts/:id/like', (req:Request, res:Response) => {
+app.put('/posts/:id/like', async (req:Request, res:Response) => {
     try {
         // const auth = req.headers.authorization as string;
         const id = req.params.id;
@@ -232,7 +245,24 @@ app.put('/posts/:id/like', (req:Request, res:Response) => {
         const creatorId = req.body.creator.id as string | undefined;
         const creatorName = req.body.creator.name as string | undefined;
 
-        const post = posts.find((post) => post.id === id);
+        const post = await db("posts").where({id: id});
+
+        if(post){
+            await db.raw(`
+                UPDATE posts
+                SET 
+                    id = "${newId || post.id}",
+                    content = "${content || post.content}",
+                    likes = "${likes || post.likes}",
+                    dislikes = "${dislikes || post.dislikes}",
+                    createdAt = "${createdAt || post.createdAt}",
+                    updatedAt = "${updatedAt || post.updatedAt}",
+                    creatorId = "${creatorId || post.creatorId}",
+                    creatorName = "${creatorName || post.creatorName}"
+                WHERE
+                    id = "${id}";
+            `)
+        }
         
     } catch (error:any) {
         console.log(error);
